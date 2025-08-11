e659fa21cf5ed2fc97868acdc80597a0b0567f1f
lluuonak <idonhava@gmail.com>
2025-08-11 11:49:59 +0900

    11시 49분 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Test1148 from './assets/Test1148';
 import Test99 from './Test99';
 
 const App = () => {
     return (
         <div>
             <Test99 />
+            <Test1148 />
         </div>
     );
 };
---
+++ src/assets/Test1148.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test1148 = () => {
+    return <div></div>;
+};
+
+export default Test1148;
